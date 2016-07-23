673c59d980fce8a43b4fba9c5a648d604db53de6
Bob <broketech@nym.hush.com>
2016-07-23 16:40:34 -0700

    Add private calls

--- node.btce.js
+++ node.btce.js
@@ -1,81 +1,181 @@
 const base_url = 'https://btc-e.com/api/2/';
 const v3_url = 'https://btc-e.com/api/3/';
-const api_key = 'APIKEY';
-const api_secret = 'APISECRET';
+const tapi_url = 'https://btc-e.com/tapi';
+const api_key = 'API_KEY';
+const api_secret = 'API_SECRET';
 
-var hmac = require('./hmac-sha512.js');
 var request = require('request');
+var qstring = require('querystring');
+var hmac = require('./hmac-sha512.js');
+
+
 
 // public methods
-  module.exports.depth = function(market, cb){  // market, callback
-    var options = {
-      url: base_url+market+'/depth'
-    };
-    pubrequest(options, function(sdata){
-        if(sdata){
-          return cb(sdata);
-        };
-    });
-  }
-  module.exports.ticker = function(market, cb){  // market, callback
-    var options = {
-      url: base_url+market+'/ticker'
-    };
-    pubrequest(options, function(sdata){
-        if(sdata){
-          return cb(sdata);
-        };
-    });
-  }
-  module.exports.trades = function(market, cb){  // market, callback
-    var options = {
-      url: base_url+market+'/trades'
-    };
-    pubrequest(options, function(sdata){
-        if(sdata){
-          return cb(sdata);
-        };
-    });
-  }
-  module.exports.info = function info(cb){  // callback
-    var options = {
-      url: v3_url+'info'
-    };
-    pubrequest(options, function(sdata){
-        if(sdata){
-          return cb(sdata);
-        };
-    });
-  }
-
+module.exports.depth = function(market, cb){  // market, callback
+  var options = {
+    url: base_url+market+'/depth'
+  };
+  pubrequest(options, function(sdata){
+      if(sdata){
+        return cb(sdata);
+      };
+  });
+}
+module.exports.ticker = function(market, cb){  // market, callback
+  var options = {
+    url: base_url+market+'/ticker'
+  };
+  pubrequest(options, function(sdata){
+      if(sdata){
+        return cb(sdata);
+      };
+  });
+}
+module.exports.trades = function(market, cb){  // market, callback
+  var options = {
+    url: base_url+market+'/trades'
+  };
+  pubrequest(options, function(sdata){
+      if(sdata){
+        return cb(sdata);
+      };
+  });
+}
+module.exports.fee = function(market, cb){  // market, callback
+  var options = {
+    url: base_url+market+'/fee'
+  };
+  pubrequest(options, function(sdata){
+      if(sdata){
+        return cb(sdata);
+      };
+  });
+}
+module.exports.info = function info(cb){  // callback
+  var options = {
+    url: v3_url+'info'
+  };
+  pubrequest(options, function(sdata){
+      if(sdata){
+        return cb(sdata);
+      };
+  });
+}
 // private methods
-/*  btce.prototype.getinfo = function(cb)
-  btce.prototype.trade = function(pair, type, rate, amount, cb)
-  btce.prototype.activeorders = function(pair, cb)
-  btce.prototype.orderinfo = function(order_id, cb)
-  btce.prototype.cancelorder = function(order_id, cb)
-  btce.prototype.tradehistory = function(params, cb)
-  btce.prototype.transhistory = function(params, cb)
-  btce.prototype.withdrawcoin = function(coinName, amount, address, cb)
-  btce.prototype.createcoupon = function(currency, amount, cb)
-  btce.prototype.redeemcoupon = function(coupon, cb)
-*/
-function privrequest(options, cb){
-  //console.log(options)
-  request(
-    options, function(error, response, body){
+module.exports.getinfo = function(params, cb){
+  var hashme = qstring.stringify(params)+"&method=getInfo";
+  var signed = hmac.HmacSHA512(hashme, api_secret).toString(hmac.enc.Hex);
+  privrequest(hashme, signed, function(sdata){
+      if(sdata){
+        return cb(sdata);
+      };
+  });
+}
+module.exports.trade = function(params, cb){
+  var hashme = qstring.stringify(params)+"&method=Trade";
+  var signed = hmac.HmacSHA512(hashme, api_secret).toString(hmac.enc.Hex);
+  privrequest(hashme, signed, function(sdata){
+      if(sdata){
+        return cb(sdata);
+      };
+  });
+}
+module.exports.activeorders = function(params, cb){
+  var hashme = qstring.stringify(params)+"&method=ActiveOrders";
+  var signed = hmac.HmacSHA512(hashme, api_secret).toString(hmac.enc.Hex);
+  privrequest(hashme, signed, function(sdata){
+      if(sdata){
+        return cb(sdata);
+      };
+  });
+}
+module.exports.orderinfo = function(params, cb){
+  var hashme = qstring.stringify(params)+"&method=OrderInfo";
+  var signed = hmac.HmacSHA512(hashme, api_secret).toString(hmac.enc.Hex);
+  privrequest(hashme, signed, function(sdata){
+      if(sdata){
+        return cb(sdata);
+      };
+  });
+}
+module.exports.cancelorder = function(params, cb){
+  var hashme = qstring.stringify(params)+"&method=CancelOrder";
+  var signed = hmac.HmacSHA512(hashme, api_secret).toString(hmac.enc.Hex);
+  privrequest(hashme, signed, function(sdata){
+      if(sdata){
+        return cb(sdata);
+      };
+  });
+}
+module.exports.tradehistory = function(params, cb){
+  var hashme = qstring.stringify(params)+"&method=TradeHistory";
+  var signed = hmac.HmacSHA512(hashme, api_secret).toString(hmac.enc.Hex);
+  privrequest(hashme, signed, function(sdata){
+      if(sdata){
+        return cb(sdata);
+      };
+  });
+}
+module.exports.transhistory = function(params, cb){
+  var hashme = qstring.stringify(params)+"&method=TransHistory";
+  var signed = hmac.HmacSHA512(hashme, api_secret).toString(hmac.enc.Hex);
+  privrequest(hashme, signed, function(sdata){
+      if(sdata){
+        return cb(sdata);
+      };
+  });
+}
+module.exports.withdrawcoin = function(params, cb){
+  var hashme = qstring.stringify(params)+"&method=WithdrawCoin";
+  var signed = hmac.HmacSHA512(hashme, api_secret).toString(hmac.enc.Hex);
+  privrequest(hashme, signed, function(sdata){
+      if(sdata){
+        return cb(sdata);
+      };
+  });
+}
+module.exports.createcoupon = function(params, cb){
+  var hashme = qstring.stringify(params)+"&method=CreateCoupon";
+  var signed = hmac.HmacSHA512(hashme, api_secret).toString(hmac.enc.Hex);
+  privrequest(hashme, signed, function(sdata){
+      if(sdata){
+        return cb(sdata);
+      };
+  });
+}
+module.exports.redeemcoupon = function(params, cb){
+  var hashme = qstring.stringify(params)+"&method=RedeemCoupon";
+  var signed = hmac.HmacSHA512(hashme, api_secret).toString(hmac.enc.Hex);
+  privrequest(hashme, signed, function(sdata){
+      if(sdata){
+        return cb(sdata);
+      };
+  });
+}
+// private function call
+function privrequest(hash, sign, cb){
+  var privDefaults = request.defaults({
+    method: "POST",
+    form: hash,
+    headers: {
+      'Key': api_key,
+      'Sign': sign
+    }
+  });
+  privDefaults(tapi_url, function(error, response, body){
       if(error){
-        return error;
+        return (error, response.statusCode);
       };
       if(!error && response.statusCode == 200){
         var data = JSON.parse(body);
         //JSON.parse(body);
+
         cb(data);
       };
     }
   );
 }
-
+// public function call
 function pubrequest(options, cb){
   //console.log(options)
   request(
